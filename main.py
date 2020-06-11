2a0e9a2ff7d02db3feea1d09cc1ec094e58821cd
sud7749 <sameersharma078626@gmail.com>
2020-06-11 14:13:16 +0800

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask,render_template,request, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+import os
 
 app = Flask(__name__) 
+app.config['SECRET_KEY']='secret'
+app.config['SQLALCHEMY_DATABASE_URL']=os.environ.get("DATABASE_URL")
 
 @app.route("/") 
 def home_view(): 
