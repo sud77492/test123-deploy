f914da3e21e182273e1a1e7729b3fffcc310b3a5
sud7749 <sameersharma078626@gmail.com>
2020-06-11 17:49:56 +0800

    first commit

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ app = Flask(__name__)
 app.config['SECRET_KEY']='secret'
 app.config['SQLALCHEMY_DATABASE_URL']=os.environ.get("DATABASE_URL")
 
-db=SQLAlchemy(app)
+# db=SQLAlchemy(app)
 
-class User(db.Model):
-	__tablename__='users'
+# class User(db.Model):
+# 	__tablename__='users'
 
-	id=db.Column(db.Integer, primary_key=True)
-	name=db.Column(db.String, primary_key=True)
-	email=db.Column(db.String, primary_key=True)
+# 	id=db.Column(db.Integer, primary_key=True)
+# 	name=db.Column(db.String, primary_key=True)
+# 	email=db.Column(db.String, primary_key=True)
 
-db.init_app()
+# db.init_app()
 
 @app.route("/") 
 def home_view(): 
