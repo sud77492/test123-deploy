98b7b12a4b3f6de4aaa069d08ee17d7e3fa1b8d4
sud7749 <sameersharma078626@gmail.com>
2020-06-11 14:26:43 +0800

    first commitexit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request, url_for
 # from flask_sqlalchemy import SQLAlchemy
 
-# import os
+import os
 
 app = Flask(__name__) 
 app.config['SECRET_KEY']='secret'
