d6ede8671eff5c8a9c0eebafe34983b42ecc06ad
sud7749 <sameersharma078626@gmail.com>
2020-06-11 17:39:07 +0800

    first commitexit

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY']='secret'
 app.config['SQLALCHEMY_DATABASE_URL']=os.environ.get("DATABASE_URL")
 
+class User(db.Model):
+	__tablename__='users'
+
+	id=db.Column(db.Integer, primary_key=True)
+	name=db.Column(db.String, primary_key=True)
+	email=db.Column(db.String, primary_key=True)
+
+db.init_app()
+
 @app.route("/") 
 def home_view(): 
 		return "<h1>Sudhanshu</h1>"
